d9cc766b49afd9369c770b75afdce7df60bedfa1
PrernaJha0 <146425924+PrernaJha0@users.noreply.github.com>
2023-10-02 23:16:25 +0530

    Create AbsoluteDifferenceOf2DiagonalSum.cs

---
+++ Algorithms/AbsoluteDifferenceOf2DiagonalSum.cs
@@ -0,0 +1,61 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+    /*
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int diagonalDifference(List<List<int>> matrix)
+    {
+        int primaryDiagonalSum = 0;
+        int secondaryDiagonalSum = 0;
+        int rowCount = matrix.Count;
+        
+        for (int i = 0; i < rowCount; i++)
+        {
+            primaryDiagonalSum += matrix[i][i];
+            secondaryDiagonalSum += matrix[i][rowCount - 1 - i];
+        }
+        
+        return Math.Abs(primaryDiagonalSum - secondaryDiagonalSum);
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<List<int>> arr = new List<List<int>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
+        }
+
+        int result = Result.diagonalDifference(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
